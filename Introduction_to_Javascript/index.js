a7b2a849a8c63b999bbab97fe63aefcfe3eafbe4
gzbsingh <39863817+gzbsingh@users.noreply.github.com>
2022-10-12 10:27:39 +0530

    Add files via upload

---
+++ Introduction_to_Javascript/index.js
@@ -0,0 +1,19 @@
+let name = "Ramu";
+let age = 23;
+let grade = "A";
+let school = "Rajamoli School";
+let Math = 98;
+let science = 89;
+let English = 99;
+let Roll_No = 123456;
+console.log("|| Name-", name, "|| Age-", age, "|| School_Name", school, "||");
+console.log("----------------------------------------------------", "||");
+console.log("|| Subject || Max. Mark || Marks", "||");
+console.log("-------------------------------------");
+console.log("|| Math    ||   100        ||", Math, "||")
+console.log("---------------------------------||");
+console.log("|| Science ||  100         ||", science, "||")
+
+console.log("---------------------------------||");
+console.log("|| English ||  100         ||", English, "||");
+console.log("---------------------------------||");
